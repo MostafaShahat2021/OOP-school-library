f41d0c97c2aaa53cde9ed17c57cda9e610054bbc
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-11-09 10:47:47 +0200

    Fix linters errors

--- person.rb
+++ person.rb
@@ -1,24 +1,27 @@
 class Person
-  attr_reader :id, :name, :age
-  attr_writer :name, :age
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  private
-  def  of_age?
-    @age >= 18
-  end
-
   def can_use_services?
     of_age? || @parent_permission
   end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
 end
 
+# EX
 mostafa = Person.new(18, 'Mostafa', false)
 puts mostafa.can_use_services? # => true
-mostafa_2 = Person.new(15, 'Mostafa', false)
-puts mostafa_2.can_use_services?  # => false
+jone = Person.new(15, 'Mostafa', false)
+puts jone.can_use_services? # => false
