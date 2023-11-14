6b7fded2836222b5071273f4072fafdb52d7d8d6
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-11-14 10:56:23 +0200

    Fix rubocop errors

--- person.rb
+++ person.rb
@@ -5,6 +5,7 @@ class Person < Nameable
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -25,4 +26,3 @@ class Person < Nameable
     @age >= 18
   end
 end
-
