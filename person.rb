0b55874822f4f5e7509cbc4ceedae1224ae5b290
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-11-17 13:33:37 +0200

    Comment Example

--- person.rb
+++ person.rb
@@ -37,9 +37,9 @@ class Person < Nameable
 end
 
 # Example
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+# person = Person.new(22, 'maximilianus')
+# person.correct_name
+# capitalized_person = CapitalizeDecorator.new(person)
+# puts capitalized_person.correct_name
+# capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+# puts capitalized_trimmed_person.correct_name
